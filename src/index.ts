467351479a84f9b6616a202227cb8e7209961384
aroramrinaal <apply.mrinaal@gmail.com>
2024-09-14 22:11:44 -0700

    changes

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,22 @@ import { Hono } from 'hono'
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/', async(c) => {
+  const res = await fetch(
+    "https://github.com/Azure-Samples/cognitive-services-speech-sdk/raw/master/samples/cpp/windows/console/samples/enrollment_audio_katie.wav"
+  );
+  const blob = await res.arrayBuffer();
+
+  const input = {
+    audio: [...new Uint8Array(blob)],
+  };
+
+  const response = await c.env.AI.run(
+    "@cf/openai/whisper-tiny-en",
+    input
+  );
+
+  return Response.json({ input: { audio: [] }, response });
 })
 
 export default app
