9bde113b2f907c9e8e75e60b1482bce2dfdd607c
aroramrinaal <apply.mrinaal@gmail.com>
2024-09-15 01:11:08 -0700

    some error

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Hono } from 'hono'
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
 app.post('/transcribe', async (c) => {
+  
   const formData = await c.req.formData()
   const audioFile = formData.get('audio') as File | null
 
@@ -11,22 +12,25 @@ app.post('/transcribe', async (c) => {
   }
 
   const arrayBuffer = await audioFile.arrayBuffer()
+
   const input = {
     audio: [...new Uint8Array(arrayBuffer)],
-  }
+  };
 
-  try {
-    const response = await c.env.AI.run(
-      "@cf/openai/whisper-tiny-en",
-      input
-    )
 
-    return c.json({ transcription: response.text,
-      word_count: response.word_count
-     })
-  } catch (error) {
-    return c.json({ error: 'Transcription failed' }, 500)
+  try{
+  const response = await c.env.AI.run(
+    "@cf/openai/whisper-tiny-en",
+    input
+  );
+  return c.json({ transcription: response.text,
+   })
+} catch (error) {
+  if (error instanceof Error) {
+    return c.json({ error: error.message }, 500)
   }
+  return c.json({ error: 'An unknown error occurred' }, 500)
+}
 })
 
 export default app
